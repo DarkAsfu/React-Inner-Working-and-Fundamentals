bd2b727dec024a6c42bd04208f54ef8cd2afe459
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-18 00:44:49 +0600

    Convert JSX To React Element Using Babel

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-// console.log("Install React Manually");
-// console.log("React =>", React);
-// console.log("ReactDOM =>", ReactDOM);
+// const HelloWorld = () => { 
+//     return React.createElement(
+//         'p',
+//         {},
+//         "HelloWorld"
+//     )
+// }
 
-// const root = document.getElementById("root");
-// root.innerText = "Hello Ashraful!"
-// creating JSX element
-const App = React.createElement('h1', {style: { color: 'red' }}, 'Hello Ashraful!')
+// const App = () => {
+//     return React.createElement(
+//         'h1',
+//         { style: {color: 'blue'} },
+//         'Hello Ashraful!',
+//         HelloWorld(),
+//     )
+// };
+const HelloWorld = () =>{
+    return (
+        <h1>Hello World</h1>
+    )
+} 
+const App = () => {
+    return (
+        <React.Fragment>
+            <p>Hello, Ashraful!</p>
+            <HelloWorld/>
+        </React.Fragment>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(App);
+root.render(React.createElement(App));
