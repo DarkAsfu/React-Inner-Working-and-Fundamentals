98a439520f9a0d6355b577848b9b341953687dc9
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-01-18 00:18:40 +0600

    Create a root and render first react element

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-console.log("Install React Manually");
-console.log("React =>", React);
-console.log("ReactDOM =>", ReactDOM);
+// console.log("Install React Manually");
+// console.log("React =>", React);
+// console.log("ReactDOM =>", ReactDOM);
 
+// const root = document.getElementById("root");
+// root.innerText = "Hello Ashraful!"
+// creating JSX element
+const App = React.createElement('h1', {style: { color: 'red' }}, 'Hello Ashraful!')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App);
